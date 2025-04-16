2969ac8e9df9f16788fe011cac9a204029d4d3f0
Mdabdulaziz <mdabdulaziz7322@gmail.com>
2025-04-16 23:59:55 +0200

    final updated

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,11 @@ setTotal(remainingTotal)
 setFavorite(remainingFavorite)
   }
 
-
+const deleteAll = () => {
+  setFavorite([])
+  setTotal(0)
+  toast("All favorites removed")
+}
   
 
   const notify = () => toast("Added to Favorite");
@@ -54,17 +58,21 @@ setFavorite(remainingFavorite)
         </div>
         <div className="favorite-section w-[30%] bg-white mt-27 rounded-2xl shadow-sm p-8 h-full ">
           <div className='flex items-center justify-center gap-3 border-b-1 border-gray-200 pb-5'>
-            <MdFavoriteBorder size={25} />
+            < MdFavoriteBorder size={25} />
             <h3 className='text-2xl py-4'>Favorite Items</h3>
           </div>
           <div>
             <Favorite favorite={favorite} handleRemoveFavorite= {handleRemoveFavorite}></Favorite>
           </div>
-          <div className='flex items-center justify-around gap-3  mt-3'>
+          <div className='flex items-center justify-around mt-3'>
 
             <h3 className='text-xl py-4'>Total bids Amount</h3>
             <p>${total}</p>
           </div>
+          <div className='text-center mt-3'>
+            <button onClick ={ () => {deleteAll()}} className="btn btn-active btn-secondary">Delete All</button>
+            </div>
+          
         </div>
 
         <ToastContainer />
